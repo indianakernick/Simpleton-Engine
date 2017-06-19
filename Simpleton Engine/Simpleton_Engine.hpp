e85cc843630cf95279a607345e8a52ae436302ab
Kerndog73 <kerndog73@gmail.com>
2017-06-19 17:57:01 +0930

    Initial Commit

---
+++ Simpleton Engine/Simpleton_Engine.hpp
@@ -0,0 +1,23 @@
+/*
+ *  Simpleton_Engine.hpp
+ *  Simpleton Engine
+ *
+ *  Created by Indi Kernick on 19/6/17.
+ *  Copyright © 2017 Indi Kernick. All rights reserved.
+ *
+ */
+
+#ifndef Simpleton_Engine_
+#define Simpleton_Engine_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class Simpleton_Engine
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ Simpleton Engine/Simpleton_EnginePriv.hpp
@@ -0,0 +1,19 @@
+/*
+ *  Simpleton_EnginePriv.hpp
+ *  Simpleton Engine
+ *
+ *  Created by Indi Kernick on 19/6/17.
+ *  Copyright © 2017 Indi Kernick. All rights reserved.
+ *
+ */
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class Simpleton_EnginePriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
