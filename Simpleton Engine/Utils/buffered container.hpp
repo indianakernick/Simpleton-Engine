3fb8df318717af3159dd460416dff7424750f333
Kerndog73 <kerndog73@gmail.com>
2017-07-24 18:06:03 +0930

    Add Utils::BufferedMapContainer
    I see this pattern quite often so I thought I'd put it in a class.

---
+++ Simpleton Engine/Utils/buffered container.hpp
@@ -0,0 +1,80 @@
+//
+//  buffered container.hpp
+//  Simpleton Engine
+//
+//  Created by Indi Kernick on 23/7/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef engine_utils_buffered_container_hpp
+#define engine_utils_buffered_container_hpp
+
+#include <vector>
+
+//@TODO Think of a better name
+
+namespace Utils {
+  class ModificationError : std::logic_error {
+  public:
+    inline ModificationError()
+      : std::logic_error("Container modification error") {}
+  };
+
+  template <typename Container>
+  class BufferedMapContainer {
+  public:
+    
+    template <typename ...Args>
+    void add(Args &&... args) {
+      if (modifying) {
+        addedElements.emplace(std::forward<Args>(args)...);
+      } else {
+        container.emplace(std::forward<Args>(args)...);
+      }
+    }
+    
+    void rem(const Container::key_type key) {
+      if (modifying) {
+        removedElements.emplace(container.find(key));
+      } else {
+        container.erase(key);
+      }
+    }
+    
+    const Container &view() const {
+      return container;
+    }
+    
+    Container &startModifying() {
+      if (modifying) {
+        throw ModificationError();
+      }
+      modifying = true;
+    }
+    
+    void stopModifying() {
+      if (!modifying) {
+        throw ModificationError();
+      }
+      modifying = false;
+      
+      for (auto e = removedElements.cbegin(); e != removedElements.cbegin(); ++e) {
+        container.erase(*e);
+      }
+      removedElements.clear();
+      
+      for (auto e = addedElements.cbegin(); e != addedElements.cend(); ++e) {
+        container.emplace(std::move(*e));
+      }
+      addedElements.clear();
+    }
+  
+  private:
+    Container container;
+    std::vector<Container::iterator> removedElements;
+    std::vector<Container::value_type> addedElements;
+    bool modifying = false;
+  };
+}
+
+#endif
