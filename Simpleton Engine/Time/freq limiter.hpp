47e869f7266230b24eaefcd9f8b3f538e956b7e7
Kerndog73 <kerndog73@gmail.com>
2017-10-16 14:10:09 +1030

    Make frequency apart of the Time::FreqLimiter interface

--- Simpleton Engine/Time/freq limiter.hpp
+++ Simpleton Engine/Time/freq limiter.hpp
@@ -13,34 +13,40 @@
 
 namespace Time {
   class OpPerSec {};
+  class Frequency {};
   
   constexpr OpPerSec OP_PER_SEC {};
+  constexpr Frequency FREQUENCY {};
 
   ///Limits the frequency of some operation in real-time
   template <typename Duration>
   class RealFreqLimiter {
   public:
     RealFreqLimiter()
-      : duration(0),
+      : period(0),
         lastDo(getPoint<Duration>()) {}
     template <typename Int>
     explicit RealFreqLimiter(const Int count)
-      : duration(count),
-        lastDo(getPoint<Duration>() - duration) {}
+      : period(count),
+        lastDo(getPoint<Duration>() - period) {}
+    template <typename Int>
+    RealFreqLimiter(Frequency, const Int count)
+      : period(1.0f / static_cast<float>(count)),
+        lastDo(getPoint<Duration>() - period) {}
     template <typename Int>
     RealFreqLimiter(OpPerSec, const Int count)
-      : duration((static_cast<float>(Duration::period::num) / Duration::period::den) * count),
-        lastDo(getPoint<Duration>() - duration) {}
+      : period((static_cast<float>(Duration::period::num * count) / Duration::period::den)),
+        lastDo(getPoint<Duration>() - period) {}
     ~RealFreqLimiter() = default;
     
-    ///Change the duration
-    void setDuration(const Duration newDuration) {
-      duration = newDuration;
+    ///Change the period
+    void setPeriod(const Duration newPeriod) {
+      period = newPeriod;
     }
     
-    ///Get the duration
-    Duration getDuration() const {
-      return duration;
+    ///Get the period
+    Duration getPeriod() const {
+      return period;
     }
     
     ///Get the amount of time that has passes since the last operation
@@ -55,7 +61,7 @@ namespace Time {
       Float
     >
     getProgress() const {
-      return static_cast<Float>((getPoint<Duration>() - lastDo).count()) / duration.count();
+      return static_cast<Float>((getPoint<Duration>() - lastDo).count()) / period.count();
     }
     
     ///Simulate a fake operation
@@ -65,14 +71,14 @@ namespace Time {
     
     ///Ensure that canDo will return true
     void willDo() {
-      lastDo = getPoint<Duration>() - duration;
+      lastDo = getPoint<Duration>() - period;
     }
     
     ///If this function returns true, it will not return true again until the
-    ///duration has passed
+    ///period has passed
     bool canDo() {
       const Point<Duration> now = getPoint<Duration>();
-      if (now - lastDo >= duration) {
+      if (now - lastDo >= period) {
         lastDo = now;
         return true;
       } else {
@@ -83,45 +89,47 @@ namespace Time {
     ///Similar to canDo but...
     bool canDoOverlap() {
       const Point<Duration> now = getPoint<Duration>();
-      if (now - lastDo >= duration) {
-        lastDo += duration;
+      if (now - lastDo >= period) {
+        lastDo += period;
         return true;
       } else {
         return false;
       }
     }
     
+    using CountType = uint32_t;
+    
     ///Returns the number of times the operation can be performed
-    uint64_t canDoMultiple() {
+    CountType canDoMultiple() {
       return canDoMultipleImpl<false>();
     }
     
     ///Similar to canDoMultiple but...
-    uint64_t canDoMultipleOverlap() {
+    CountType canDoMultipleOverlap() {
       return canDoMultipleImpl<true>();
     }
     
   private:
-    //minumum duration between operations
-    Duration duration;
+    //minumum period between operations
+    Duration period;
     //time of last operation
     Point<Duration> lastDo;
     
     template <bool OVERLAP>
-    uint64_t canDoMultipleImpl() {
+    CountType canDoMultipleImpl() {
       const Point<Duration> now = getPoint<Duration>();
       
-      if (duration == 0) {
+      if (period == 0) {
         if (now - lastDo == Duration(0)) {
           return 0;
         } else {
-          return std::numeric_limits<uint64_t>::max();
+          return std::numeric_limits<CountType>::max();
         }
       }
     
-      uint64_t count = 0;
-      while (now - lastDo >= duration) {
-        lastDo += duration;
+      CountType count = 0;
+      while (now - lastDo >= period) {
+        lastDo += period;
         ++count;
       }
       if constexpr (!OVERLAP) {
@@ -139,21 +147,29 @@ namespace Time {
   
   public:
     DeltaFreqLimiter()
-      : duration(0) {}
-    explicit DeltaFreqLimiter(const Number duration)
-      : duration(duration) {}                                  //assumes milliseconds
-    DeltaFreqLimiter(OpPerSec, const Number count, const Number unitsPerSec = Number(1000))
-      : duration(unitsPerSec / count) {}
+      : period(0) {}
+    explicit DeltaFreqLimiter(const Number period)
+      : period(period) {}
+    DeltaFreqLimiter(Frequency, const Number frequency)
+      : period(Number(1) / frequency) {}
     ~DeltaFreqLimiter() = default;
     
-    ///Change the duration
-    void setDuration(const Number newDuration) {
-      duration = newDuration;
+    ///Change the period
+    void setPeriod(const Number newPeriod) {
+      period = newPeriod;
+    }
+    ///Get the period
+    Number getPeriod() const {
+      return period;
     }
     
-    ///Get the duration
-    Number getDuration() const {
-      return duration;
+    ///Change the frequency
+    void setFrequency(const Number newFreq) {
+      period = Number(1) / newFreq;
+    }
+    ///Get the frequency
+    Number getFrequency() const {
+      return Number(1) / period;
     }
     
     ///Get the amount of time that has passes since the last operation
@@ -168,7 +184,7 @@ namespace Time {
       Float
     >
     getProgress() const {
-      return static_cast<Float>(timeSinceLast) / duration;
+      return static_cast<Float>(timeSinceLast) / period;
     }
     
     ///Advance time forward
@@ -183,13 +199,13 @@ namespace Time {
     
     ///Ensure that canDo will return true
     void willDo() {
-      timeSinceLast = duration;
+      timeSinceLast = period;
     }
     
     ///If this function returns true, it will not return true again until the
-    ///duration has passed
+    ///period has passed
     bool canDo() {
-      if (timeSinceLast >= duration) {
+      if (timeSinceLast >= period) {
         timeSinceLast = Number(0);
         return true;
       } else {
@@ -197,47 +213,46 @@ namespace Time {
       }
     }
     
-    ///Similar to canDo but...
+    ///Similar to canDo but there are no gaps between periods
     bool canDoOverlap() {
-      if (timeSinceLast >= duration) {
-        timeSinceLast -= duration;
+      if (timeSinceLast >= period) {
+        timeSinceLast -= period;
         return true;
       } else {
         return false;
       }
     }
     
-    using CountType = std::conditional_t<std::is_integral<Number>::value, Number, uint32_t>;
+    using CountType = uint32_t;
     
     ///Returns the number of times the operation can be performed
     CountType canDoMultiple() {
       return canDoMultipleImpl<false>();
     }
     
-    ///Similar to canDoMultiple but...
+    ///Similar to canDoMultiple but there are no gaps between periods
     CountType canDoMultipleOverlap() {
       return canDoMultipleImpl<true>();
     }
     
   private:
-    //minumum duration between operations
-    Number duration;
+    //minumum period between operations
+    Number period;
     //time since last operation
     Number timeSinceLast = Number(0);
     
     template <bool OVERLAP>
     CountType canDoMultipleImpl() {
-      if (duration == Number(0)) {
+      if (period == Number(0)) {
         if (timeSinceLast == Number(0)) {
           return Number(0);
         } else {
-          using Limits = std::numeric_limits<Number>;
-          return Limits::has_infinity ? Limits::infinity() : Limits::max();
+          return std::numeric_limits<CountType>::max();
         }
       }
       CountType count = 0;
-      while (timeSinceLast >= duration) {
-        timeSinceLast -= duration;
+      while (timeSinceLast >= period) {
+        timeSinceLast -= period;
         ++count;
       }
       if constexpr (!OVERLAP) {
