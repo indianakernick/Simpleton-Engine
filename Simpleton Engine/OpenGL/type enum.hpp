40bb3ab32b355dc59c287c3ae62228d2b243614d
Kerndog73 <kerndog73@gmail.com>
2017-12-19 20:16:08 +1030

    Simplify GL::TypeEnum
    I think the OpenGL helper is just about done. This is orders of magnitude better than the abstraction I wrote in April in the old Game Engine.

--- Simpleton Engine/OpenGL/type enum.hpp
+++ Simpleton Engine/OpenGL/type enum.hpp
@@ -10,12 +10,9 @@
 #define engine_opengl_type_enum_hpp
 
 #include "opengl.hpp"
-#include <type_traits>
+#include "../Utils/meta glm.hpp"
 
 namespace GL {
-  template <typename T>
-  struct TypeEnum {};
-  
   #define TYPE_ENUM(TYPE, ENUM)                                                 \
     template <>                                                                 \
     struct TypeEnum<TYPE> {                                                     \
@@ -23,36 +20,13 @@ namespace GL {
       static constexpr GLsizei size = 1;                                        \
     };
   
-  #define VECTOR_TYPE_ENUM(TYPE, SIZE)                                          \
-    template <>                                                                 \
-    struct TypeEnum<TYPE> {                                                     \
-      static constexpr GLenum type = TypeEnum<typename TYPE::value_type>::type; \
-      static constexpr GLsizei size = SIZE;                                     \
-    };
-  
-  #define MATRIX_TYPE_ENUM(TYPE, ROWS, COLS)                                    \
-    template <>                                                                 \
-    struct TypeEnum<TYPE> {                                                     \
-      static constexpr GLenum type = TypeEnum<typename TYPE::value_type>::type; \
-      static constexpr GLsizei rows = ROWS;                                     \
-      static constexpr GLsizei cols = COLS;                                     \
-    };
-  
-  #define MULTI_VECTOR_TYPE_ENUM(SIZE)                                          \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<bool>, SIZE)                               \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLbyte>, SIZE)                             \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLubyte>, SIZE)                            \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLshort>, SIZE)                            \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLushort>, SIZE)                           \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLint>, SIZE)                              \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLuint>, SIZE)                             \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLfloat>, SIZE)                            \
-    VECTOR_TYPE_ENUM(glm::tvec##SIZE<GLdouble>, SIZE)                           \
+  template <typename Vector>
+  struct TypeEnum {
+    static_assert(Utils::is_vec<Vector>);
+    static constexpr GLenum type = TypeEnum<Utils::vec_value_type<Vector>>::type;
+    static constexpr GLsizei size = Utils::vec_size<Vector>;
+  };
 
-  #define MULTI_MATRIX_TYPE_ENUM(ROWS, COLS)                                    \
-    MATRIX_TYPE_ENUM(glm::tmat##ROWS##x##COLS<GLfloat>, ROWS, COLS)             \
-    MATRIX_TYPE_ENUM(glm::tmat##ROWS##x##COLS<GLdouble>, ROWS, COLS)
-    
   //GLboolean is defined as a uint8_t so the types conflict
   
   TYPE_ENUM(bool, GL_BOOL)
@@ -65,25 +39,7 @@ namespace GL {
   TYPE_ENUM(GLfloat, GL_FLOAT)
   TYPE_ENUM(GLdouble, GL_DOUBLE)
   
-  MULTI_VECTOR_TYPE_ENUM(2)
-  MULTI_VECTOR_TYPE_ENUM(3)
-  MULTI_VECTOR_TYPE_ENUM(4)
-  
-  MULTI_MATRIX_TYPE_ENUM(2, 2)
-  MULTI_MATRIX_TYPE_ENUM(2, 3)
-  MULTI_MATRIX_TYPE_ENUM(2, 4)
-  MULTI_MATRIX_TYPE_ENUM(3, 2)
-  MULTI_MATRIX_TYPE_ENUM(3, 3)
-  MULTI_MATRIX_TYPE_ENUM(3, 4)
-  MULTI_MATRIX_TYPE_ENUM(4, 2)
-  MULTI_MATRIX_TYPE_ENUM(4, 3)
-  MULTI_MATRIX_TYPE_ENUM(4, 4)
-  
   #undef TYPE_ENUM
-  #undef VECTOR_TYPE_ENUM
-  #undef MATRIX_TYPE_ENUM
-  #undef MULTI_VECTOR_TYPE_ENUM
-  #undef MULTI_MATRIX_TYPE_ENUM
 }
 
 #endif
