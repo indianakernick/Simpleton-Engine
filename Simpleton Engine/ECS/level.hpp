03ab5c6b2578a4aacf9ef040e2eb47ab2ba093bc
Kerndog73 <kerndog73@gmail.com>
2017-12-30 18:02:07 +1030

    Add progress manager

---
+++ Simpleton Engine/ECS/level.hpp
@@ -0,0 +1,16 @@
+//
+//  level.hpp
+//  Simpleton Engine
+//
+//  Created by Indi Kernick on 30/12/17.
+//  Copyright © 2017 Indi Kernick. All rights reserved.
+//
+
+#ifndef engine_ecs_level_hpp
+#define engine_ecs_level_hpp
+
+namespace ECS {
+  using Level = unsigned;
+}
+
+#endif
