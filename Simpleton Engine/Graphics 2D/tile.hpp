1621bcdabe3d29f2b3bbb93962f0a080865bb4c7
Kerndog73 <kerndog73@gmail.com>
2018-02-22 16:10:02 +1030

    Add doc comments to G2D::tilePos and G2D::tileTex

--- Simpleton Engine/Graphics 2D/tile.hpp
+++ Simpleton Engine/Graphics 2D/tile.hpp
@@ -12,7 +12,11 @@
 #include "types.hpp"
 
 namespace G2D {
+  /// Write positions of verticies on a quad assuming that the quad is an
+  /// axis-aligned rectangle
   void tilePos(Quad &, float depth, glm::vec2 pos, glm::vec2 size = {1.0f, 1.0f});
+  /// Write texture coordinates of vertices on a quad assuming that the texture
+  /// is sampled as an axis-aligned rectangle
   void tileTex(Quad &, TexCoordType pos, TexCoordType size = {1.0f, 1.0f});
 }
 
