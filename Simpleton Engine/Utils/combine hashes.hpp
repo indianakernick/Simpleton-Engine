fceb33dc9ed439c72805faf3256104de59a552aa
Kerndog73 <kerndog73@gmail.com>
2018-03-06 08:07:27 +1030

    Add 32-bit support to Utils::combineHashes

--- Simpleton Engine/Utils/combine hashes.hpp
+++ Simpleton Engine/Utils/combine hashes.hpp
@@ -13,16 +13,20 @@
 
 namespace Utils {
   constexpr size_t combineHashes(const size_t left, const size_t right) {
-    static_assert(sizeof(size_t) == 8, "Only 64 bit hashes can be combined");
-  
     //https://stackoverflow.com/a/8980550
     
-    const size_t mul = 0x9E3779B97F4A7C15ull;
-    size_t a = (right ^ left) * mul;
-    a ^= (a >> 47);
-    size_t b = (left ^ a) * mul;
-    b ^= (b >> 47);
-    return b * mul;
+    if constexpr (sizeof(size_t) == 8) {
+      // CityHash
+      const size_t mul = 0x9E3779B97F4A7C15ull;
+      size_t a = (right ^ left) * mul;
+      a ^= (a >> 47);
+      size_t b = (left ^ a) * mul;
+      b ^= (b >> 47);
+      return b * mul;
+    } else if constexpr (sizeof(size_t) == 4) {
+      // boost hash_combine
+      return left ^ (right + 0x9E3779B9 + (left << 6) + (left >> 2));
+    }
   }
 }
 
