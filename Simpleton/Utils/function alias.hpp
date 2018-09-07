96df734269336c2869e3f6352e2cefb3d08b86cc
Kerndog73 <kerndog73@gmail.com>
2018-09-07 11:41:45 +0930

    Make function alias SFINAE friendly
    https://codereview.stackexchange.com/q/202791/117677

--- Simpleton/Utils/function alias.hpp
+++ Simpleton/Utils/function alias.hpp
@@ -18,41 +18,14 @@
 /// Create a function alias with specifiers
 #define FUN_ALIAS_SPEC(SPECS, NEW_NAME, ...)                                    \
   template <typename... Args>                                                   \
-  SPECS decltype(auto) NEW_NAME(Args &&... args)                                \
-    noexcept(noexcept(__VA_ARGS__(std::forward<Args>(args)...))) {              \
+  SPECS auto NEW_NAME(Args &&... args)                                          \
+    noexcept(noexcept(__VA_ARGS__(std::forward<Args>(args)...)))                \
+    -> decltype(__VA_ARGS__(std::forward<Args>(args)...)) {                     \
     return __VA_ARGS__(std::forward<Args>(args)...);                            \
   }
 
 /// Create a function alias
 #define FUN_ALIAS(NEW_NAME, ...)                                                \
   FUN_ALIAS_SPEC(inline, NEW_NAME, __VA_ARGS__)
-
-/// Create a function alias and forward template type parameters with specifiers
-#define FUN_ALIAS_TMP_TYPE_SPEC(SPECS, NEW_NAME, ...)                           \
-  template <typename... Params, typename... Args>                               \
-  SPECS decltype(auto) NEW_NAME(Args &&... args)                                \
-    noexcept(noexcept(__VA_ARGS__<Params...>(std::forward<Args>(args)...))) {   \
-    return __VA_ARGS__<Params...>(std::forward<Args>(args)...);                 \
-  }
-
-/// Create a function alias and forward template value parameters with specifiers
-#define FUN_ALIAS_TMP_VAL_SPEC(SPECS, NEW_NAME, ...)                            \
-  template <auto... Params, typename... Args>                                   \
-  SPECS decltype(auto) NEW_NAME(Args &&... args)                                \
-    noexcept(noexcept(__VA_ARGS__<Params...>(std::forward<Args>(args)...))) {   \
-    return __VA_ARGS__<Params...>(std::forward<Args>(args)...);                 \
-  }
-
-/// Create a function alias using a lambda with specifiers
-#define FUN_ALIAS_LAM_SPEC(SPECS, NEW_NAME, ...)                                            \
-  SPECS auto NEW_NAME = [] (auto &&... args)                                    \
-    noexcept(noexcept(__VA_ARGS__(std::forward<decltype(args)>(args)...)))      \
-    -> decltype(auto) {                                                         \
-    return __VA_ARGS__(std::forward<decltype(args)>(args)...);                  \
-  };
-
-/// Create a function alias using a lambda
-#define FUN_ALIAS_LAM(NEW_NAME, ...)                                            \
-  FUN_ALIAS_LAM_SPEC(constexpr, NEW_NAME, __VA_ARGS__)
 
 #endif
